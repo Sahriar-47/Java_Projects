be8f096d1479ed832d7e1b27b924ddb69c3b0d60
Sahriar <sahriar22047@gmail.com>
2025-07-15 00:39:37 +0600

    connect jdbc with servlet

---
+++ Servlet-jsp/src/main/java/DBConnection.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnection {
+    protected Connection connection;
+    protected PreparedStatement ps;
+    protected ResultSet rs;
+
+    public DBConnection() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/servletDB";
+        String username = "root";
+        String password = "sahriar#";
+        try {
+            connection = DriverManager.getConnection(url, "root", "sahriar#");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void closeAll() {
+        try{
+            if(this.rs != null){
+                this.rs.close();
+            }
+            if(this.ps != null){
+                this.ps.close();
+            }
+            if(this.connection != null){
+                this.connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
